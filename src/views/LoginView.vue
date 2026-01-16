d3726d6c774398bbb1555651dddfbddef47175a1
kimu805 <kimukimuok805@gmail.com>
2026-01-16 16:55:54 +0900

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { login as loginApi } from "@/api/auth"
+
+  const email = ref("")
+  const password = ref("")
+  const error = ref("")
+
+  const auth = useAuthStore()
+  const router = useRouter()
+
+  const handleLogin = async () => {
+    error.value = ""
+
+    try { 
+      const res = await loginApi(email.value, password.value)
+      auth.login(res.token)
+      router.push("/")
+    } catch (e) {
+      error.value = "ログインに失敗しました"
+    }
+  }
+</script>
+
 <template>
-  <h1>LoginView</h1>
+  <h1>ログイン</h1>
+
+  <div v-if="error" style="color: red">{{ error }}</div>
+
+  <label>email
+    <input v-model="email" placeholder="email">
+  </label>
+
+  <label>password
+    <input v-model="password" type="password" placeholder="password">
+  </label>
+
+  <button @click="handleLogin">ログイン</button>
 </template>
